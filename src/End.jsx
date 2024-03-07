49808f572fa722039779330178c614baf5c02f06
JeffreyDinackus <jeffreydinackus@proton.me>
2024-03-06 20:03:49 -0500

    added links for stack badges in end component and margin

--- src/End.jsx
+++ src/End.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import 'bulma/css/bulma.min.css';
 import jsLogo from './assets/js.svg'
+import netlifyLogo from './assets/netlify.svg'
 
 function End() {
   return (
@@ -13,10 +14,17 @@ function End() {
         <div className="column is-one-third mt-4">
           <p className="has-text-centered has-text-dark">Built with: </p>
           <div className="is-flex is-justify-content-center"> {/* Center the images */}
-            <img className="image is-32x32" src={reactLogo} alt="React Logo" /> {/* Use Bulma 'image' class */}
-            <img className="image is-32x32" src={jsLogo} alt="JS Logo" />
 
-            <img className="image is-32x32" src={viteLogo} alt="Vite Logo" />
+
+
+            <a href="https://react.dev/"><img className="image is-32x32 end-badge" src={reactLogo} alt="React Logo" /></a>
+
+
+            <a href="https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web/JavaScript_basics"><img className="image is-32x32 end-badge" src={jsLogo} alt="JS Logo" /></a>
+
+            <a href="https://vitejs.dev/"><img className="image is-32x32 end-badge" src={viteLogo} alt="Vite Logo" /></a>
+
+            <a href="https://www.netlify.com/"><img className="image is-32x32 end-badge" src={netlifyLogo} alt="Netlify Logo" /></a>
 
           </div>
         </div>
