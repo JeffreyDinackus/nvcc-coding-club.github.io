11dbb4ab9d92ba127b06a491c40a4582f9b50bd0
JeffreyDinackus <jeffreydinackus@proton.me>
2024-03-09 13:20:16 -0500

    fixing button clipping

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -36,9 +36,9 @@ function Navbar() {
           {/* link to notion */}
           <div className="end navbar-end">
 
-            <a href="https://cypress-pixie-354.notion.site/Home-b632fa70517646c9bca0dea8e63e5ad8"><button className="button-nav button is-info">Notion</button></a>
+            <a href="https://cypress-pixie-354.notion.site/Home-b632fa70517646c9bca0dea8e63e5ad8"><button className="button-nav  is-mobile button is-info">Notion</button></a>
             <a href="https://www.linkedin.com/company/nvcc-coding-club/
-"><button className="button-nav button is-success">LinkedIn</button></a>
+"><button className="button-nav button is-success is-mobile">LinkedIn</button></a>
           </div>
 
 
